16df6fea51f3065f56fe3fd19b661f5b37233703
krkchaitanya <krkchaitanya42@gmail.com>
2017-09-30 17:51:57 -0500

    thirdCommit

--- models/user.js
+++ models/user.js
@@ -30,3 +30,19 @@ module.exports.createUser=function(newUser,callback){
     });
 });
 };
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.comparePassword=function(candidatePassword,hash,callback){
+  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+   if(err) throw err;
+   callback(null,isMatch);
+});
+};
+
+module.exports.getUserById=function(id,callback){
+  User.findById(id,callback);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const router = express.Router();
-var User=require("../models/user");
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
 
 router.get("/register",(req,res)=>{
   res.render('register');
@@ -43,5 +45,49 @@ if(errors){
   req.flash("success_msg","You are registered and can now login");
   res.redirect('/users/login');
 }
-})
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+   User.getUserByUsername(username, function(err, user){
+   	if(err) throw err;
+   	if(!user){
+   		return done(null, false, {message: 'Unknown User'});
+   	}
+
+   	User.comparePassword(password, user.password, function(err, isMatch){
+   		if(err) throw err;
+   		if(isMatch){
+   			return done(null, user);
+   		} else {
+   			return done(null, false, {message: 'Invalid password'});
+   		}
+   	});
+   });
+  }));
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+router.post('/login',
+  passport.authenticate('local',{successRedirect:"/",failureRedirect:'/users/login',failureFlash:true}),
+  function(req, res) {
+      res.redirect('/');
+  });
+
+  router.get('/logout', function(req, res){
+  	req.logout();
+
+  	req.flash('success_msg', 'You are logged out');
+
+  	res.redirect('/users/login');
+  });
+
 module.exports=router;
