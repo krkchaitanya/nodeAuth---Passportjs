c159ed214161bb27359d5c8bc1528173bf868063
krkchaitanya <krkchaitanya42@gmail.com>
2017-09-30 18:07:39 -0500

    fourthCommit

--- app.js
+++ app.js
@@ -68,8 +68,11 @@ app.use(function(req,res,next){
   res.locals.success_msg=req.flash('success_msg');
   res.locals.error_msg=req.flash('error_msg');
   res.locals.error=req.flash('error');
+  res.locals.user=req.user||null;
   next();
 });
+
+
 
 app.use("/",routes);
 app.use("/users",users);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/",(req,res)=>{
+router.get("/",ensureAuthenticated,(req,res)=>{
   res.render('index');
 });
+
+function ensureAuthenticated(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }else{
+    //req.flash("error_msg","you are not logged in");
+    res.redirect('/users/login');
+  }
+};
 
 module.exports=router;
